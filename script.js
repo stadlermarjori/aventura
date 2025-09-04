8c806e1ebe22644834b8f17fb7c461b0abe1f07d
stadlermarjori <stadlermarjori@gmail.com>
2025-09-04 13:43:15 +0000

    aula5

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const avanca = document.querySelectorAll('.btn-proximo');
-avanca.forEach(button =>){
-    button.addEventLister('click, function(){
-     const atual = document.querySelectorAll('.ativo');
-     const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
 
-     atual.classList.remove('ativo');
-     document.getElementById(proximoPasso).classList.add('ativo');
-        
-            
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-     }
-
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
